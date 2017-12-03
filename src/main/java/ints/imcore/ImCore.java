134105e8f60ae1be270962d295181f1eafceeb2d
int soumen <codeinfer@gmail.com>
2017-12-03 19:00:54 +0530

    New Commit
    OpenCV Face recognition.

---
+++ src/main/java/ints/imcore/ImCore.java
@@ -0,0 +1,9 @@
+package ints.imcore;
+
+/**
+ * Created by soumensardar on 03/12/17.
+ */
+
+public class ImCore {
+
+}
---
+++ src/main/obj/local/mips64/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,137 @@
+/Users/soumensardar/Documents/Projects/AndroidStudioProjects/MyBaby/app/src/main/obj/local/mips64/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  /Users/soumensardar/Documents/Projects/AndroidStudioProjects/MyBaby/app/src/main/jni/DetectionBasedTracker_jni.cpp \
+  /Users/soumensardar/Documents/Projects/AndroidStudioProjects/MyBaby/app/src/main/jni/DetectionBasedTracker_jni.h \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cv_cpu_dispatch.h \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/vsx_utils.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/ovx.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+  /Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+  /Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+/Users/soumensardar/Documents/Projects/AndroidStudioProjects/MyBaby/app/src/main/jni/DetectionBasedTracker_jni.h:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cv_cpu_dispatch.h:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/vsx_utils.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/core/ovx.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/soumensardar/OpenCV/OpenCV-3.3.1-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/soumensardar/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
